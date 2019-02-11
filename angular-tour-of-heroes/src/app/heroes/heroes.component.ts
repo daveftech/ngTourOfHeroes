584224fa6a9fb8f072e4d574df1347f633fe19ed
David Fardoe <dfardoe@winnipeg.ca>
2019-02-11 11:34:55 -0600

    Set up the list of mock heroes and display it

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../_models/hero';
+import { HEROES } from '../_models/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +8,11 @@ import { Hero } from '../_models/hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   hero: Hero = {
     id: 1,
     name: 'Wolverine'
   };
+  heroes = HEROES;
 
   constructor() { }
 
