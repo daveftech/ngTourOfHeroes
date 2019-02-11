7bd37f6185afd0c6de63cd7b40f77689f996c68c
David Fardoe <dfardoe@winnipeg.ca>
2019-02-11 15:55:48 -0600

    Added Add/Update/Delete functionality

--- angular-tour-of-heroes/src/app/_services/hero.service.ts
+++ angular-tour-of-heroes/src/app/_services/hero.service.ts
@@ -12,6 +12,7 @@ import { MessageService } from './message.service';
 export class HeroService {
   private heroesUrl = 'api/heroes';
 
+
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
@@ -23,12 +24,51 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return this.http.get<Hero>(`${this.heroesUrl}/${id}`)
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.get<Hero>(url)
       .pipe(
         tap(_ => this.log(`Fetched hero id=${id}`)),
         catchError(this.handleError<Hero>(`getHero id: ${id}`))
+      );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        tap((newHero: Hero) => this.log(`Added Hero with ID: ${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    return this.http.put(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        tap(_ => this.log(`Updated Hero ID: ${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+
+    const url = `${this.heroesUrl}/${id}`;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    return this.http.delete<Hero>(url, httpOptions)
+      .pipe(
+        tap(_ => this.log(`Deleted Hero ID: ${id}`)),
+        catchError(this.handleError<Hero>('deleteHero'))
       );
   }
 
