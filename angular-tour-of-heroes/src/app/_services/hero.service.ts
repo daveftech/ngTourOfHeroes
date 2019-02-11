7ab5492af7163cae2ede3cd06b41e46a133299f7
David Fardoe <dfardoe@winnipeg.ca>
2019-02-11 12:09:11 -0600

    Created MessageService and MessagesComponent

--- angular-tour-of-heroes/src/app/_services/hero.service.ts
+++ angular-tour-of-heroes/src/app/_services/hero.service.ts
@@ -3,15 +3,18 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from '../_models/hero';
 import { HEROES } from '../_models/mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -7,12 +7,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './_services/hero.service';
+import { MessageService } from './_services/message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +23,8 @@ import { HeroService } from './_services/hero.service';
     FormsModule
   ],
   providers: [
-    HeroService
+    HeroService,
+    MessageService
   ],
   bootstrap: [AppComponent]
 })
